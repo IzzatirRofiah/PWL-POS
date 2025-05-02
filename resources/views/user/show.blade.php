21cee5b4d1946bda782e7a89195ef9801dbcc99a
Izzatir Rofi'ah <zaizza294@gmail.com>
2025-05-02 07:26:44 +0700

    JS 02

---
+++ resources/views/user/show.blade.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+    <h1>Hello, Izzatir Rofi'ah</h1>
+    <h1>ID 15</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SalesController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// JS 02
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/sales', [SalesController::class, 'index'])->name('sales.index');
