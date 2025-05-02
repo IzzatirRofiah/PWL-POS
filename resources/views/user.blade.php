2226eececdce880768437306fcdfbcf08b1174ea
Izzatir Rofi'ah <zaizza294@gmail.com>
2025-05-02 10:22:39 +0700

    JS04-Prak2.3

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -30,7 +30,7 @@
     <title>Data User</title>
 </head>
 <body>
-    <h1>Data User</h1>
+    {{-- <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
             <th>ID</th>
@@ -44,6 +44,17 @@
             <td>{{ $data->nama }}</td>
             <td>{{ $data->level_id }}</td>
         </tr>
+    </table> --}}
+
+    <h1>Data User</h1>
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <th>Jumlah Pengguna</th>
+        </tr>
+        <tr>
+            <td>{{ $data }}</td>
+        </tr>
     </table>
+
 </body>
 </html>
