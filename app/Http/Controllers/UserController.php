9587ce8e01881cf4713f9aff5b853cda711cd1ed
Izzatir Rofi'ah <zaizza294@gmail.com>
2025-05-02 11:08:05 +0700

    JS05-Prak2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -191,14 +191,14 @@
     // }
 // }
 
-namespace App\Http\Controllers;
+// namespace App\Http\Controllers;
 
-use App\Models\UserModel;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
+// use App\Models\UserModel;
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Hash;
 
-class UserController extends Controller
-{
+// class UserController extends Controller
+// {
     // public function index()
     // {
     //     $user = UserModel::firstOrCreate(
@@ -225,21 +225,78 @@ class UserController extends Controller
     //     return view('user', ['data' => $data]);
     // }
 
+//     public function index()
+//     {
+//         $user = UserModel::firstOrCreate(
+//             [
+//                 'username' => 'manager33',
+//                 'nama' => 'Manager Tiga Tiga',
+//                 'password' => Hash::make('12345'), 
+//                 'level_id' => 2
+//             ]
+//         );
+
+//         $user->save();
+
+//         $data = [$user]; 
+//         return view('user', ['data' => $data]);
+//     }
+
+// }
+
+namespace App\Http\Controllers;
+
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    // public function index()
+    // {
+    //     $user = UserModel::create([
+    //         'username' => 'manager55',
+    //         'nama' => 'Manager 55',
+    //         'password' => Hash::make('12345'),
+    //         'level_id' => 2,
+    //     ]);
+
+    //     $user->username = 'manager56';
+
+    //     $user->isDirty(); // true
+    //     $user->isDirty('username'); // true
+    //     $user->isDirty('nama'); // false
+    //     $user->isDirty(['nama', 'username']); // true
+
+    //     $user->isClean(); // false
+    //     $user->isClean('username'); // false
+    //     $user->isClean('nama'); // true
+    //     $user->isClean(['nama', 'username']); // false
+
+    //     $user->save();
+
+    //     $user->isDirty(); // false
+    //     $user->isDirty(); // false
+    //     dd($user->isDirty());
+    // }
+
     public function index()
     {
-        $user = UserModel::firstOrCreate(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'), 
-                'level_id' => 2
-            ]
-        );
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+
+        $user->username = 'manager12';
 
         $user->save();
 
-        $data = [$user]; 
-        return view('user', ['data' => $data]);
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); // true
+        $user->wasChanged('nama'); // false
+        dd($user->wasChanged(['nama', 'username'])); // true
     }
-
 }
