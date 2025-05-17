2463accaea904de6c851fb360741dd7b23dd277b
IzzatirRofiah <zaizza294@gmail.com>
2025-05-17 15:58:58 +0700

    Praktikum 3 – Implementasi jQuery Datatable di AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,38 @@ Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 Route::get('/user/hapus/{id}', [UserController::class,'hapus']);
 
 Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);              // menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);          // menampilkan data user dalam bentuk json untuk datatables 
+    Route::get('/create', [UserController::class, 'create']);       // menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']);             // menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);           // menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);      // menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);         // menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']);     // menghapus data user
+});
+
+Route::group(['prefix' => 'level'], function () {
+    Route::get('/', [LevelController::class, 'index']);             // Menampilkan halaman awal level user
+    Route::post('/list', [LevelController::class, 'list']);         // menampilkan level user dalam bentuk json untuk datatables
+    Route::get('/create', [LevelController::class, 'create']);      // menampilkan halaman form tambah level user
+    Route::post('/', [LevelController::class, 'store']);            // menyimpan level user baru
+    Route::get('/{id}', [LevelController::class, 'show']);          // Menampilkan detail level user
+    Route::get('/{id}/edit', [LevelController::class, 'edit']);     // menampilkan halaman form edit level user
+    Route::put('/{id}', [LevelController::class, 'update']);        // menyimpan perubahan level user
+    Route::delete('/{id}', [LevelController::class, 'destroy']);    // menghapus level user
+});
+
+Route::group(['prefix' => 'kategori'], function () {
+    Route::get('/', [KategoriController::class, 'index']);          // Menampilkan halaman awal daftar kategori
+    Route::post('/list', [KategoriController::class, 'list']);      // menampilkan kategori dalam bentuk json untuk datatables
+    Route::get('/create', [KategoriController::class, 'create']);   // menampilkan halaman form tambah kategori
+    Route::post('/', [KategoriController::class, 'store']);         // menyimpan kategori baru
+    Route::get('/{id}', [KategoriController::class, 'show']);       // menampilkan detail kategori
+    Route::get('/{id}/edit', [KategoriController::class, 'edit']);  // menampilkan halaman form edit kategori
+    Route::put('/{id}', [KategoriController::class, 'update']);     // menyimpan perubahan kategori
+    Route::delete('/{id}', [KategoriController::class, 'destroy']); // menghapus kategori
+});
+
+
